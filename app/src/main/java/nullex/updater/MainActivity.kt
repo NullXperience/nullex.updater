ed00f132495878bca82cd53ee00379b3096b404b
peachpixels <309287626+makimaa-thr1z@users.noreply.github.com>
2026-08-09 19:53:17 +0530

    app: mimic colorOS updater app

---
+++ app/src/main/java/nullex/updater/MainActivity.kt
@@ -0,0 +1,13 @@
+package nullex.updater
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        supportFragmentManager.beginTransaction().setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+            .replace(R.id.ThisFragmentContainer, NotFound()).addToBackStack(null).commit()
+    }
+}
